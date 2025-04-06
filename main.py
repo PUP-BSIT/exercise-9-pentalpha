ad00a78fa3389cf336be8b218fdb3f308061f40f
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2025-04-06 20:03:05 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# TODO: (Bautista) Create the menu of choices and a switch case statement 
+# that matches the options in the menu.
+
+# TODO: (Bautista) Create the list all functionality and print the contents in
+# a pleasant and formatted way
+
+# TODO: (Managbanag) Create a function that will add new records to the list
+
+# TODO: (Espinola) Create a search function that returns the index 
+# of the found item
+
+# TODO: (Banzali) Create an update function to change the 
+# fields of the chosen record
+
+# TODO: (Raymundo) Create a delete function that deletes the chosen record
